46a3d0385e41a056f3302f57de22c4e823718e98
Hunter Martin <175766562+huntermar3@users.noreply.github.com>
2025-12-28 17:23:44 -0500

    routing to login page from home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,18 +1,26 @@
 import "../css/Home.css"
+import { useNavigate } from 'react-router-dom';
+
 function Home() {
+  const navigate = useNavigate();
+
+  function redirectToLogin() {
+    navigate('/login');
+  }
+  
   return <>
     <div className="main-container">
       <div className="home-page-content">
-        
+
         <div className="title-container">
           <h1 className="title">DRUMMOND</h1>
           <hr></hr>
         </div>
 
         <div className = "game-options-container">
-          <button className="game-option-button">STARTING 5 BATTLES</button>
-          <button className="game-option-button">GRID</button>
-          <button className="game-option-disabled-button">COMING SOON</button>
+          <button className="game-option-button" onClick={redirectToLogin}>STARTING 5 BATTLES</button>
+          <button className="game-option-button" onClick={redirectToLogin}>GRID</button>
+          <button className="game-option-disabled-button" disabled={true}>COMING SOON</button>
         </div>
 
         <p className="footer">DETROIT BASKETBALL</p>
