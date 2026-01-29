b2e3303d85b0a22e8b96572dadc1503cc3a356ea
Charles <cthorn@emich.edu>
2026-01-29 13:09:35 -0500

    some sql

--- backend/Data/Repositories/Leaderboard.cs
+++ backend/Data/Repositories/Leaderboard.cs
@@ -2,11 +2,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using backend.Models;
+using Dapper;
 
 namespace backend.Data.Repositories
 {
     public class Leaderboard : ILeaderboard
     {
-        
-    }
+        private readonly Database _database;
+
+        public Leaderboard(Database database)
+        {
+            _database = database;
+        }
+
+        private async Task<IEnumerable<User>> GetAllUsers()
+        {
+            var sql = "SELECT * FROM leaderboard";
+            using var connection = _database.GetConnection();
+            return await connection.QueryAsync<User>(sql);           
+        }
 }
